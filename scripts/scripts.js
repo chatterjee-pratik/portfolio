542045e8d7b0c7582d0ad47c4f2144d32550deda
chatterjee-pratik <172855546+chatterjee-pratik@users.noreply.github.com>
2024-08-29 12:00:56 +0200

    Update scripts.js
    Update Profile Picture Link
    
    Signed-off-by: chatterjee-pratik <172855546+chatterjee-pratik@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -83,7 +83,7 @@ document.querySelector("#contact-section iframe").addEventListener("load", funct
     document.getElementById("profile-image").style.setProperty("display", "none");
     const aboutMeSection = document.getElementById("about-me-section");
     const profileImage = document.createElement("img");
-    profileImage.src = "https://media.licdn.com/dms/image/D4E03AQFzxkDZQVzL3w/profile-displayphoto-shrink_800_800/0/1719313514606?e=1724889600&v=beta&t=DFRlRmtt1yPDrQY_1UWggxgZY-mn33lBzPXahHSiPlY";
+    profileImage.src = "https://media.licdn.com/dms/image/v2/D4E03AQFzxkDZQVzL3w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719313514606?e=1730332800&v=beta&t=2A9GgQdescSgbBWODTJn6-RBr6AiFuQc0eS0bROcBjQ";
     profileImage.style.setProperty("width", "10rem");
     profileImage.style.setProperty("height", "10rem");
     profileImage.style.setProperty("border-radius", "50%");
